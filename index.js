5367a87e85670f99bc30309934872b5b5534a403
sindiga-dev <isaac.sindiga@student.moringaschool.com>
2022-08-27 21:26:18 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+// Add your code here
+function submitData(name, email) {
+	let users = {
+		name: name,
+		email: email,
+	};
+	return fetch("http://localhost:3000/users", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			Accept: "application/json",
+		},
+		body: JSON.stringify(users),
+	})
+		.then((response) => response.json())
+		.then((fetchedobject) => {
+			document.body.innerHTML = fetchedobject["id"];
+		})
+		.catch((error) => {
+			document.body.innerHTML = error.message;
+		});
+}
